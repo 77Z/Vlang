70bf55cec620916fcd11647c17aa25d94fb86c82
77Z <46038462+77Z@users.noreply.github.com>
2020-10-19 14:46:07 -0500

    Create vlang.h

---
+++ vlang.h
@@ -0,0 +1,16 @@
+#ifndef _VLANG_
+#define _VLANG_
+
+#include <iostream>
+#include <string>
+
+//main function
+#define main int main(
+
+//void function
+#define voidfunc void
+
+void print(std::string text);
+void println(std::string text);
+
+#endif
