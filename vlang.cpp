5dc613e04855816e5607b3d10e4ee18e7af95dcc
77Z <46038462+77Z@users.noreply.github.com>
2020-10-19 14:47:22 -0500

    Create vlang.cpp

---
+++ vlang.cpp
@@ -0,0 +1,12 @@
+#include "vlang.h"
+
+#include <iostream>
+#include <string>
+
+void print(std::string text) {
+  std::cout << text;
+}
+
+void println(std::string text) {
+  std::cout << text << std::endl;
+}
